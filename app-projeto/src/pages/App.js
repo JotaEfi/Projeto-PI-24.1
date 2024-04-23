dfc6914286e1b9fc54058e9d642b2352d483c1e8
JotaEfi <joaodaviabc@gmail.com>
2024-04-23 19:51:41 -0300

    second structural commit

---
+++ app-projeto/src/pages/App.js
@@ -0,0 +1,11 @@
+import './styles/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ app-projeto/src/pages/home.js
@@ -0,0 +1,10 @@
+import "../styles/scss/home.scss";
+import "../styles/css/index.css";
+function Home(){
+    <div className="home">
+        <p>Test</p>
+    </div>
+
+}
+
+export default Home;
